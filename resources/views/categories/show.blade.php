3edf52f85e8aa8d6f19ed3fdc84f71040e83f578
jprice <jprice@izeni.com>
2025-05-26 00:09:09 -0600

    Fix failing example test by setting up a .env.testing env file

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Category: {{ $category->name }}</h1>
-<p>Created at: {{ $category->created_at->format('Y-m-d H:i:s') }}</p>
-<p>Updated at: {{ $category->updated_at->format('Y-m-d H:i:s') }}</p>
-<a href="{{ route('index') }}">Back to Categories</a>
+    <h1>Category: {{ $category->name }}</h1>
+    <p>Created at: {{ $category->created_at->format('Y-m-d H:i:s') }}</p>
+    <p>Updated at: {{ $category->updated_at->format('Y-m-d H:i:s') }}</p>
+    <a href="{{ route('index') }}">Back to Categories</a>
 @endsection
